a79b2be30a0da331b89758d381e2cd97d8c35ac3
quit <hj.gonzalez23@uniandes.edu.co>
2014-01-11 13:07:56 -0500

    Finalizacion CRUD Usuaru
    CRUD

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,3 +1,18 @@
 class Usuario < ActiveRecord::Base
   belongs_to :perfil
+  validate :valida_fecha_nacimiento
+  validates :usr_tipo_docum_ident, presence: {message: "=> Es un dato requerido"}
+  validates :usr_nro_docum_ident, presence: {message: "=> Es un dato requerido"}, numericality: true, uniqueness:true
+  validates :usr_nombres, presence: {message: "=> Es un dato requerido"}, length: { in: 2..40, too_short: "=> Los nombres deben tener entre 2 y 40 caracteres", too_long:"=> Los nombres deben tener entre 2 y 40 caracteres"}
+  validates :usr_apellidos, presence: {message: "=> Es un dato requerido"}, length: { in: 2..40, too_short: "=> Los apellidos deben tener entre 2 y 40 caracteres", too_long:"=> Los apellidos deben tener entre 2 y 40 caracteres"}
+  validates :usr_correo_electronico, uniqueness: true
+  validates :usr_genero, presence: {message: "=> Es un dato requerido"}
+  validates :usr_estado, presence: {message: "=> Es un dato requerido"}
+  validates :perfil_id, presence: {message: "=> Es un dato requerido"}, numericality: true
+  
+  def valida_fecha_nacimiento
+    if usr_fecha_nacimiento.present? && usr_fecha_nacimiento > Date.today then
+      errors.add(:usr_fecha_nacimiento, "La fecha de nacimiento no puede ser posterior a la fecha actual")
+    end 
+  end
 end
